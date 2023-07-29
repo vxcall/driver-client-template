e729e738bc6a3d0f9a8da56e44957cfd62d52930
pseuxide <s3pt3mb3r@protonmail.com>
2023-07-29 16:55:55 +0900

    make everything snake_case for my readability

--- driver/driver_entry.cpp
+++ driver/driver_entry.cpp
@@ -1,47 +1,48 @@
 #include "driver_entry.h"
 #include <ntddk.h>
 
-VOID driverUnload(PDRIVER_OBJECT DriverObject) {
-    UNREFERENCED_PARAMETER(DriverObject);
+VOID driver_unload(PDRIVER_OBJECT driver_object) {
+    UNREFERENCED_PARAMETER(driver_object);
 }
 
-NTSTATUS DriverCreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp);
+NTSTATUS driver_create_close(PDEVICE_OBJECT device_object, PIRP irp);
 
-extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
-    UNREFERENCED_PARAMETER(RegistryPath);
-    DriverObject->DriverUnload = driverUnload;
-    DriverObject->MajorFunction[IRP_MJ_CREATE] = DriverCreateClose;
-    DriverObject->MajorFunction[IRP_MJ_CLOSE] = DriverCreateClose;
-    UNICODE_STRING deviceName = RTL_CONSTANT_STRING(deviceNameW);
+extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT driver_object, PUNICODE_STRING registry_path) {
+    UNREFERENCED_PARAMETER(registry_path);
+    driver_object->DriverUnload = driver_unload;
+    driver_object->MajorFunction[IRP_MJ_CREATE] = driver_create_close;
+    driver_object->MajorFunction[IRP_MJ_CLOSE] = driver_create_close;
+    UNICODE_STRING device_name = RTL_CONSTANT_STRING(device_name_w);
 
-    PDEVICE_OBJECT DeviceObject;
+    PDEVICE_OBJECT device_object;
     NTSTATUS status = IoCreateDevice(
-            DriverObject,
+            driver_object,
             0,
-            &deviceName,
+            &device_name,
             FILE_DEVICE_UNKNOWN,
             0,
             FALSE,
-            &DeviceObject);
+            &device_object);
 
     if (!NT_SUCCESS(status)) {
         KdPrint(("Failed to create device object (0x%08X)\n", status));
         return status;
     }
-    UNICODE_STRING symLink = RTL_CONSTANT_STRING(symNameW);
-    status = IoCreateSymbolicLink(&symLink, &deviceName);
+
+    UNICODE_STRING sym_link = RTL_CONSTANT_STRING(sym_name_w);
+    status = IoCreateSymbolicLink(&sym_link, &device_name);
     if (!NT_SUCCESS(status)) {
         KdPrint(("Failed to create symbolic link (0x%08X)\n", status));
-        IoDeleteDevice(DeviceObject); // undo every step
+        IoDeleteDevice(device_object); // undo every step
         return status;
     }
     return STATUS_SUCCESS;
 }
 
-NTSTATUS DriverCreateClose(PDEVICE_OBJECT DeviceObject, PIRP Irp) {
-    UNREFERENCED_PARAMETER(DeviceObject);
-    Irp->IoStatus.Status = STATUS_SUCCESS; // The value this request should be completed with.
-    Irp->IoStatus.Information = 0;         // polymophic member. different things in different request. Create and Close are fine with 0.
-    IoCompleteRequest(Irp, IO_NO_INCREMENT); // IO_NO_INCREMENT means zero.
+NTSTATUS driver_create_close(PDEVICE_OBJECT device_object, PIRP irp) {
+    UNREFERENCED_PARAMETER(device_object);
+    irp->IoStatus.Status = STATUS_SUCCESS; // The value this request should be completed with.
+    irp->IoStatus.Information = 0;         // polymophic member. different things in different request. Create and Close are fine with 0.
+    IoCompleteRequest(irp, IO_NO_INCREMENT); // IO_NO_INCREMENT means zero.
     return STATUS_SUCCESS;
 }
--- driver/driver_entry.h
+++ driver/driver_entry.h
@@ -1,2 +1,2 @@
-#define deviceNameW L"\\Device\\DEVICE_NAME"
-#define symNameW L"\\DosDevices\\DEVICE_NAME"
+#define device_name_w L"\\Device\\DEVICE_NAME"
+#define sym_name_w L"\\DosDevices\\DEVICE_NAME"
